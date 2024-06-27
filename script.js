5bab8a54040d9f08a55d1a64b5b1ca24634865f6
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-06-27 17:10:48 +0400

    add display none on #cookie

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ cookies_btn.addEventListener("click", () => {
 
 function setCookie(cName, cValue, expDays) {
   let date = new Date();
-  date.setTime(date.getTime() + expDays * 24 * 60 * 60 * 1000);
+  date.setDate(date.getDate() + expDays * 24 * 60 * 60 * 1000);
   const expires = "expires" + date.toUTCString();
   document.cookie = cName + "=" + cValue + ";" + expires + " path=/";
 }
